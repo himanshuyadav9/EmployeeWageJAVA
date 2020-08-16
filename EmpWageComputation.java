1518aa86fe8f41b99c3f08af0c4f2a10a30d1069
Himanshu <yadavhimanshu63@gmail.com>
2020-08-16 19:10:25 +0530

    Employee Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,19 +1,29 @@
 public class EmpWageComputation
 {
-    public static final int IS_FULL_TIME= 1;
-
-    public static void main(String[]args)
-    {
+      //constants
+     public static final int FULL_TIME_EMP= 1;
+     public static final int EMP_RATE_PER_HOUR=20;
+    
+     public static void main(String[]args)
+     {
       System.out.println("welcome to employee wage computation program ");
-     
-      double empCheck =(int)((Math.random()*10)%2);
-      if (empCheck==IS_FULL_TIME)
-      {
-      System.out.println("Employee is present");
-      }
-      else
-      {
-      System.out.println("Employee is Absent");
-      }
-    }
+       
+       //variables
+       int empHrs=0;
+       int empWage=0;
+ 
+       int randomCheck =(int)((Math.random()*10)%2);
+       if (FULL_TIME_EMP==randomCheck)
+       {
+         System.out.println("employee is present");
+         empHrs=8;
+       }
+       else
+       {
+        System.out.println("employee is Absent");
+        empHrs=0;
+       }
+       empWage=empHrs*EMP_RATE_PER_HOUR;
+       System.out.println("employee wage:" +empWage);
+   }
 }
